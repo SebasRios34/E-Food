6b8d211016bb7059e991de7389ee1acbb5ddc862
amonteroa <amonteroa548@ulacit.ed.cr>
2019-11-19 16:53:31 -0800

    Prueba validaciones en inicio de sesion

--- ProyectoWeb/E-Food/JS/inicioSesion.js
+++ ProyectoWeb/E-Food/JS/inicioSesion.js
@@ -1,3 +1,4 @@
+/*
 (function () {
     // var init =()=>{
     //     var modal = document.getElementById('id01');
@@ -51,4 +52,42 @@
     //init();
     carousel();
     //backgroundPics();
+    
 })()
+*/
+
+var init =(e) =>{
+    e.preventDefault();
+    const usuario = document.getElementById('usuario')
+    const password = document.getElementById('password')
+    const form = document.getElementById('form')
+    const errorElement = document.getElementById('error')
+    form.addEventListener('submit', (e) => {
+        let messages = []
+        if(usuario.value === '' || usuario.value === null){
+            messages.push('El nombre de usuario es requerido')
+        }
+        if(password.value === '' || password.value === null){
+            messages.push('la contraseña es requerida')
+        }
+        if(password.value === 'password'){
+            messages.push('la contraseña no puede ser password')
+        }
+        if(password.value ==='constraseña'){
+            messages.push('la contraseña no puede ser contraseña')
+        } 
+    
+        if(messages.length > 0){
+            e.preventDefault()
+            errorElement.innerText = messages.join(', ')
+        }
+    })
+}
+
+window.onload = function () {
+    
+    var btnIngresar =  document.getElementById('btnIngresar');
+    btnIngresar.onclick = init;
+
+}
+    
