77ff3086307dec3eb593a603e443a0541fa68e6f
andres <amonteroa548@ulacit.ed.cr>
2019-11-19 22:32:24 -0600

    implementacion de facebook API v1

--- ProyectoWeb/E-Food/JS/inicioSesion.js
+++ ProyectoWeb/E-Food/JS/inicioSesion.js
@@ -55,39 +55,38 @@
     
 })()
 */
+(function(){
 
-var init =(e) =>{
-    e.preventDefault();
-    const usuario = document.getElementById('usuario')
-    const password = document.getElementById('password')
-    const form = document.getElementById('form')
-    const errorElement = document.getElementById('error')
-    form.addEventListener('submit', (e) => {
-        let messages = []
-        if(usuario.value === '' || usuario.value === null){
-            messages.push('El nombre de usuario es requerido')
-        }
-        if(password.value === '' || password.value === null){
-            messages.push('la contraseña es requerida')
-        }
-        if(password.value === 'password'){
-            messages.push('la contraseña no puede ser password')
-        }
-        if(password.value ==='constraseña'){
-            messages.push('la contraseña no puede ser contraseña')
-        } 
+    var init =(e) =>{
+        //e.preventDefault();
+        const usuario = document.getElementById('usuario')
+        const password = document.getElementById('password')
+        const form = document.getElementById('form')
+        const errorElement = document.getElementById('error')
+        form.addEventListener('submit', (e) => {
+            let messages = []
+            if(usuario.value === '' || usuario.value === null){
+                messages.push('El nombre de usuario es requerido')
+            }
+            if(password.value === '' || password.value === null){
+                messages.push('la contraseña es requerida')
+            }
+            if(password.value === 'password'){
+                messages.push('la contraseña no puede ser password')
+            }
+            if(password.value ==='constraseña'){
+                messages.push('la contraseña no puede ser contraseña')
+            } 
+        
+            if(messages.length > 0){
+                e.preventDefault()
+                errorElement.innerText = messages.join(', ')
+            }
+        })
+    }
     
-        if(messages.length > 0){
-            e.preventDefault()
-            errorElement.innerText = messages.join(', ')
-        }
-    })
-}
+    init();
+})()
 
-window.onload = function () {
-    
-    var btnIngresar =  document.getElementById('btnIngresar');
-    btnIngresar.onclick = init;
 
-}
     
