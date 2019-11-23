af29091e3d7c59770aec0e4ae2415e3a2e00fd93
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-11-22 22:38:12 -0600

    qwerty

--- ProyectoWeb/E-Food/JS/inicioSesion.js
+++ ProyectoWeb/E-Food/JS/inicioSesion.js
@@ -1,85 +1,99 @@
-/*
 (function () {
-    // var init =()=>{
-    //     var modal = document.getElementById('id01');
-    //     var modal2 = document.getElementById('id02');
 
-    //     window.onclick = function (event) {
-    //         if (event.target == modal) {
-    //             modal.style.display = "none";
-    //         }
-
-    //         if (event.target == modal2) {
-    //             modal2.style.display = "none";
-    //         }
-    //     }
-    // }
-    // var backgroundPics =()=>{
-    //     var slideIndex = 0;
-    //     showSlides();
-
-    //     function showSlides() {
-    //         var i;
-    //         var slides = document.getElementsByClassName("mySlides");
-    //         for (i = 0; i < slides.length; i++) {
-    //             slides[i].style.display = "none";
-    //         }
-    //         slideIndex++;
-    //         if (slideIndex > slides.length) {
-    //             slideIndex = 1
-    //         }
-    //         slides[slideIndex - 1].style.display = "block";
-    //         setTimeout(showSlides, 2000);
-    //     }
-    // }
-
-    var myIndex = 0;
+    /*
+    (function () {
+        // var init =()=>{
+        //     var modal = document.getElementById('id01');
+        //     var modal2 = document.getElementById('id02');
     
-
-    var carousel = () =>
-    {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
+        //     window.onclick = function (event) {
+        //         if (event.target == modal) {
+        //             modal.style.display = "none";
+        //         }
+    
+        //         if (event.target == modal2) {
+        //             modal2.style.display = "none";
+        //         }
+        //     }
+        // }
+        // var backgroundPics =()=>{
+        //     var slideIndex = 0;
+        //     showSlides();
+    
+        //     function showSlides() {
+        //         var i;
+        //         var slides = document.getElementsByClassName("mySlides");
+        //         for (i = 0; i < slides.length; i++) {
+        //             slides[i].style.display = "none";
+        //         }
+        //         slideIndex++;
+        //         if (slideIndex > slides.length) {
+        //             slideIndex = 1
+        //         }
+        //         slides[slideIndex - 1].style.display = "block";
+        //         setTimeout(showSlides, 2000);
+        //     }
+        // }
+    
+        var myIndex = 0;
+        
+    
+        var carousel = () =>
+        {
+            var i;
+            var x = document.getElementsByClassName("mySlides");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            myIndex++;
+            if (myIndex > x.length) { myIndex = 1 }
+            x[myIndex - 1].style.display = "block";
+            setTimeout(carousel, 2000); // Change image every 2 seconds
         }
-        myIndex++;
-        if (myIndex > x.length) { myIndex = 1 }
-        x[myIndex - 1].style.display = "block";
-        setTimeout(carousel, 2000); // Change image every 2 seconds
-    }
-
-    //init();
-    carousel();
-    //backgroundPics();
     
-})()
-*/
-function validation(){
-    //e.preventDefault();
-    const usuario = document.getElementById('usuario');
+        //init();
+        carousel();
+        //backgroundPics();
+        
+    })()
+    */
+
+    var arrayDatosUsuariosAdmin = [];
+
+    var usuario = document.getElementById('usuario');
     var password = document.getElementById('password');
-    const form = document.getElementById('form');
-    const errorElement = document.getElementById('error');
-    
-    let messages = []
-    if(usuario.value === ' ' || usuario.value === null){
-        messages.push('El nombre de usuario es requerido');
-    }
-    if(password.value === ' ' || password.value === null){
-        messages.push('la contraseña es requerida');
-    }
-    if(password.innerText === 'password'){
-        messages.push('la contraseña no puede ser password');
-    }
-    if(password.value ==='constraseña'){
-        messages.push('la contraseña no puede ser contraseña');
-    } 
+    var verifica = true;
 
-    if(messages.length > 0){
-        e.preventDefault();
-        errorElement.innerText = messages.join(', ');
+    var cargarUsuarios = () => {
+        fetch('https://localhost:44360/api/UsuariosAdmin')
+            .then(res => res.json())
+            .then(data => {
+                data = JSON.parse(data)
+                arrayDatosUsuariosAdmin = data;
+                console.table(arrayDatosUsuariosAdmin)
+                validation();
+            })
+            .catch(err => console.log('error', err));
     }
-}
 
-    
+    var validation = () => {
+
+        if (arrayDatosUsuariosAdmin.indexOf(usuario) && arrayDatosUsuariosAdmin.indexOf(password)) {
+            alert('Bienvenido: ' + usuarioInput);
+            window.location.href("/ProyectoWeb/E-Food/HTML/MenuPrincipal.html");
+        } else {
+            alert('Datos Incorrectos');
+            verifica = false;
+            usuario.focus();
+            password.focus();
+        }
+    }
+
+    var btnIngresar = document.getElementById('btnIngresar');
+    btnIngresar.onclick = function () {
+        cargarUsuarios();
+    }
+
+})()
+
+
