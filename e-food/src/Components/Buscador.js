d54dbacfcf2f7a97eca857d917854897be283f4d
andres <amonteroa548@ulacit.ed.cr>
2019-12-12 12:15:54 -0600

    se continuo el buscador

--- e-food/src/Components/Buscador.js
+++ e-food/src/Components/Buscador.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
-export default class Buscador extends Component {
+class Buscador extends Component {
     constructor(props) {
         super(props);
         this.state = {value: 'refrescos'};
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     }
     
     handleChange(event) {
@@ -15,8 +16,18 @@ export default class Buscador extends Component {
     }
     
     handleSubmit(event) {
-        console.log('va a buscar: ' + this.state.value);
+        alert('esta buscando: ' + this.state.value);
+        {/*<Link to="/detalleProducto"></Link>*/}
+        if(this.state.value == 'combos'){
+        }
         event.preventDefault();
+    }
+
+    handleInput(event){
+        const target = event.target;
+        this.setState({
+            [target.name]: target.value
+        })
     }
     
     render() {
@@ -25,18 +36,23 @@ export default class Buscador extends Component {
             <label>
             elija una opcion:  
                 <select className="ml-1 mr-1" value={this.state.value} onChange={this.handleChange}>
-                    <option value="combos">Combos</option>
-                    <option value="refrescos">Refrescos</option>
-                    <option value="postres">Postres</option>
-                    <option value="algoMas">algo mas</option>
+                    <option  value="combos">Combos</option>
+                    <option  value="refrescos">Refrescos</option>
+                    <option  value="postres">Postres</option>
+                    <option  value="algoMas">algo mas</option>
                 </select>
                 <div >
                     <div className="input-group">
-                        <input id="buttondropdown" name="buttondropdown" className="form-control" type="text"></input> 
+                        <input 
+                            id="buscadorInput" 
+                            name="buscadorInput" 
+                            className="form-control" 
+                            type="text"
+                            value={this.state.value}
+                            onChange={this.handleInput}>
+                        </input> 
                     </div>
-                    <Link to="/detalleProducto">
                         <input type="submit" value="Submit" />
-                    </Link>
                 </div>
             </label>
             
@@ -45,3 +61,5 @@ export default class Buscador extends Component {
     
     }
 }
+
+export default withRouter(Buscador)
