ba78a691cabbc7a7c515f2e7f24e0df6feaad5c6
SebasRios34 <ligasebasrios@gmail.com>
2019-12-17 20:56:03 -0600

    ultimos cambios

---
+++ ProyectoWeb/E-Food/JS/JSListas/informacionTipoPrecio.js
@@ -0,0 +1,114 @@
+(function () {
+    var arrayTipoPrecio = [];
+
+    var cargarTiposPrecio = () => {
+        $.get("https://localhost:44360/api/TipoPrecio/", function (data, status) {
+            data = JSON.parse(data)
+            arrayTipoPrecio = data;
+            console.table(data);
+            console.table(arrayTipoPrecio);
+        });
+    }
+
+    var agregarTipoPrecio = () => {
+        var data =
+        {
+            "TipoConsecutivo": $('#tipoConsecutivo').val(),
+            "CodigoTipoPrecio": $('#codigoTipoPrecio'),
+            "PrecioMonto": $('#monto').val(),
+            "NombrePrecio": $('#descripcion').val()
+        }
+
+        $.ajax(
+            {
+                url: "https://localhost:44360/api/TipoPrecio",
+                type: 'POST',
+                dataType: 'json',
+                data: data,
+                success: function (a, b, xhr) {
+                    console.table('Datos insertados: ' + data);
+                    alert('Se ingreso la tabla: ' + $('#codigoTipoPrecio').val());
+                    console.table(xhr);
+                },
+                error: function (a, b, error) {
+                    alert('ERROR')
+                    console.error('ERROR' + error);
+                }
+            })
+    }
+
+    var eliminarTipoPrecio = () => {
+        var url = "https://localhost:44360/api/TipoPrecio/" + $('#codigoTipoPrecio').val();
+
+        $.ajax({
+            url: url,
+            method: 'DELETE',
+            dataType: 'json',
+            data:
+            {
+                "CodigoTipoPrecio": $('#codigoTipoPrecio').val()
+            },
+            success: function (data, text, xhr) {
+                alert('Se ha eliminado el tipo de precio: ' + $('#codigoTipoPrecio').val());
+                console.log(data);
+                console.log(text);
+                console.table(xhr);
+            },
+            error: function (xhr, text, error) {
+                console.error("Error: " + error);
+            }
+        })
+    }
+
+    var modificarTipoPrecio = () => {
+        var url = "https://localhost:44360/api/TipoPrecio/" + $('#codigoTipoPrecio').val();
+        var data =
+        {
+            "TipoConsecutivo": $('#tipoConsecutivo').val(),
+            "CodigoTipoPrecio": $('#codigoTipoPrecio'),
+            "PrecioMonto": $('#monto').val(),
+            "NombrePrecio": $('#descripcion').val()
+        }
+
+        $.ajax({
+            url: url,
+            method: 'PUT',
+            dataType: 'json',
+            data: data,
+            success: function (data, text, xhr) {
+                alert('Se ha modificado el tipo de precio');
+                console.table(data);
+                console.log(text);
+                console.table(xhr);
+            },
+            error: function (xhr, text, error) {
+                console.error("Error: " + error);
+            }
+        })
+    }
+
+    var validarTipoPrecioId = () => {
+        var codigo = $('#codigoTipoPrecio').val();
+
+        var verifica = false;
+
+        for (var i = 0; i < arrayTipoPrecio.length; i++) {
+            if (codigo != $(agregarTipoPrecio).eq(i).attr('CodigoTipoPrecio')) {
+                verifica = true;
+                console.log('verificaCodigo: ' + verifica);
+                break;
+            }
+        }
+        return verifica;
+    }
+
+    var validar = () =>{
+        
+    }
+
+    var init = () => {
+        cargarTiposPrecio();
+    }
+
+    init();
+})()
--- ProyectoWeb/E-Food/JS/informacionMetodoPago.js
+++ ProyectoWeb/E-Food/JS/informacionMetodoPago.js
@@ -50,6 +50,60 @@
          })
    }
 
+   var eliminarProcesador = () => {
+      var url = "https://localhost:44360/api/ProcesadorPago/" + $('#codigoMetodoPago').val();
+
+      $.ajax({
+         url: url,
+         method: 'DELETE',
+         dataType: 'json',
+         data:
+         {
+            "CodigoProcesador": $('#codigoMetodoPago').val()
+         },
+         success: function (data, text, xhr) {
+            alert('Se ha eliminado el procesador: ' + $('#codigoMetodoPago').val());
+            console.log(data);
+            console.log(text);
+            console.table(xhr);
+         },
+         error: function (xhr, text, error) {
+            console.error("Error: " + error);
+         }
+      })
+   }
+
+   var modificarProcesador = () => {
+      var url = "https://localhost:44360/api/ProcesadorPago/" + $('#codigoMetodoPago').val();
+      var data =
+      {
+         TipoConsecutivo: 4,
+         CodigoProcesador: $('#codigoMetodoPago').val(),
+         NombreProcesador: $('#procesador').val(),
+         Tipo: $('#tipo').val(),
+         Estado: $('#estado').val(),
+         RequiereVerificacion: $('#requiereVerificacion').val(),
+         Metodo: $('#metodo').val(),
+         CodigoTarjeta: $('#codigoTarjeta').val(),
+      }
+
+      $.ajax({
+         url: url,
+         method: 'PUT',
+         dataType: 'json',
+         data: data,
+         success: function (data, text, xhr) {
+            alert('Se ha modificado el procesador');
+            console.table(data);
+            console.log(text);
+            console.table(xhr);
+         },
+         error: function (xhr, text, error) {
+            console.error("Error: " + error);
+         }
+      })
+   }
+
    var validarProcesadorId = () => {
       var codigo = $('#codigoMetodoPago').val();
 
@@ -57,6 +111,21 @@
 
       for (var i = 0; i < arrayProcesadorPago.length; i++) {
          if (codigo != $(arrayProcesadorPago).eq(i).attr('CodigoProcesador')) {
+            verifica = true;
+            console.log('verificaCodigo: ' + verifica);
+            break;
+         }
+      }
+      return verifica;
+   }
+
+   var validarProcesadorId2 = () => {
+      var codigo = $('#codigoMetodoPago').val();
+
+      var verifica = false;
+
+      for (var i = 0; i < arrayProcesadorPago.length; i++) {
+         if (codigo == $(arrayProcesadorPago).eq(i).attr('CodigoProcesador')) {
             verifica = true;
             console.log('verificaCodigo: ' + verifica);
             break;
@@ -134,6 +203,54 @@
 
          if (verifica) {
             agregarProcesador();
+         }
+      })
+
+      $("[name='btnEliminar']").click(function (e) {
+         e.preventDefault();
+
+         var codigo = $('#codigoMetodoPago').val();
+
+         var verifica = true;
+
+         if (codigo == '') {
+            alert('Codigo Procesador es obligatorio');
+            verifica = false;
+            $('#codigoMetodoPago').focus();
+         } else if (!validarProcesadorId2()) {
+            alert('Codigo Procesador ya existe');
+            verifica = false;
+            $('#codigoMetodoPago').focus();
+         }
+
+         if (verifica) {
+            eliminarProcesador();
+         }
+      })
+
+      $("[name='btnModificar']").click(function (e) {
+         e.preventDefault();
+
+         var codigo = $('#codigoMetodoPago').val();
+
+         var verifica = true;
+
+         if (codigo == '') {
+            alert('Codigo Procesador es obligatorio');
+            verifica = false;
+            $('#codigoMetodoPago').focus();
+         } else if (!validarProcesadorId()) {
+            alert('Codigo Procesador ya existe');
+            verifica = false;
+            $('#codigoMetodoPago').focus();
+         } else if (!validarTarjetaId2()) {
+            alert('Codigo tarjeta no existe');
+            verifica = false;
+            $('#procesador').focus();
+         }
+
+         if (verifica) {
+            modificarProcesador();
          }
       })
    }
