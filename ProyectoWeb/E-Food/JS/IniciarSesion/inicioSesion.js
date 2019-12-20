59c2bde42a665e9b0e48174307a352662a793da3
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-12-20 12:49:23 -0600

    roles

--- ProyectoWeb/E-Food/JS/IniciarSesion/inicioSesion.js
+++ ProyectoWeb/E-Food/JS/IniciarSesion/inicioSesion.js
@@ -17,12 +17,12 @@
         verifica = false;
         console.table(arrayUsuariosAdmin);
 
-        for(var i = 0; i < arrayUsuariosAdmin.length; i++){
-            if((usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre')) && (contrasena == $(arrayUsuariosAdmin).eq(i).attr('Contrasena'))){
+        for (var i = 0; i < arrayUsuariosAdmin.length; i++) {
+            if ((usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre')) && (contrasena == $(arrayUsuariosAdmin).eq(i).attr('Contrasena'))) {
                 verifica = true;
                 console.log('Usuario: ' + usuarioNombre + '\nPassword: ' + contrasena);
                 break;
-            } 
+            }
         }
         return verifica;
     }
@@ -31,10 +31,10 @@
         var usuarioNombre = $('#usuario').val();
 
         verifica = false;
-        
 
-        for(var i = 0; i < arrayUsuariosAdmin.length; i++){
-            if(usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre') && 'Activo' == $(arrayUsuariosAdmin).eq(i).attr('Estado')){
+
+        for (var i = 0; i < arrayUsuariosAdmin.length; i++) {
+            if (usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre') && 'Activo' == $(arrayUsuariosAdmin).eq(i).attr('Estado')) {
                 verifica = true;
                 console.log('Usuario: ' + $('#usuario').val() + ':\nEstado: Activo');
                 break;
@@ -60,19 +60,32 @@
                 alert('Introduzca una contraseña')
                 $('#password').focus();
                 verifica = false;
-            } else if (!validarUsuarioContrasena()){
+            } else if (!validarUsuarioContrasena()) {
                 alert('Usuario: ' + usuarioNombre + ' Contraseña: ' + contrasena + '\nNo estan en la base de datos');
                 $('#usuario').focus();
                 verifica = false;
-            } else if(!validarEstado()){
-                alert('Usuario: '+ usuarioNombre + '\nSu estado se encuentra: Inactivo');
+            } else if (!validarEstado()) {
+                alert('Usuario: ' + usuarioNombre + '\nSu estado se encuentra: Inactivo');
                 $('#usuario').focus();
                 verifica = false;
             }
 
-            if(verifica){
-                alert('Bienvenido: ' + usuarioNombre);
-                $(location).attr('href', 'http://127.0.0.1:5501/ProyectoWeb/E-Food/HTML/MenuPrincipal.html')
+            if (verifica) {
+                for (var i = 0; i < arrayUsuariosAdmin.length; i++) {
+                    if ((usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre')) && 1 == $(arrayUsuariosAdmin).eq(i).attr('RolID')) {
+                        alert(usuarioNombre + " ha entrado como usuario administrativo");
+                        $(location).attr('href', 'http://127.0.0.1:5501/ProyectoWeb/E-Food/HTML/MenuPrincipal.html')
+                    } else if ((usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre')) && 2 == $(arrayUsuariosAdmin).eq(i).attr('RolID')) {
+                        alert(usuarioNombre + " ha entrado como usuario de seguridad");
+                        $(location).attr('href', 'http://127.0.0.1:5501/ProyectoWeb/E-Food/HTML/MenuPrincipalSeguridad.html')
+                    } else if ((usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre')) && 3 == $(arrayUsuariosAdmin).eq(i).attr('RolID')) {
+                        alert(usuarioNombre + " ha entrado como usuario de mantenimiento");
+                        $(location).attr('href', 'http://127.0.0.1:5501/ProyectoWeb/E-Food/HTML/MenuPrincipal.html')
+                    } else if ((usuarioNombre == $(arrayUsuariosAdmin).eq(i).attr('UsuarioNombre')) && 4 == $(arrayUsuariosAdmin).eq(i).attr('RolID')) {
+                        alert(usuarioNombre + " ha entrado como usuario de consulta");
+                        $(location).attr('href', 'http://127.0.0.1:5501/ProyectoWeb/E-Food/HTML/MenuPrincipalConsulta.html')
+                    }
+                }
             }
         })
     }
