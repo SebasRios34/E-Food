7b0c050e6dcf372dd742062a9bd4b8f5beb65051
Sebastian Rios Luna <ligasebasrios@gmail.com>
2019-12-21 05:10:53 -0600

    producto arreglo sirve

--- e-food/src/Components/Producto.js
+++ e-food/src/Components/Producto.js
@@ -75,34 +75,77 @@ export default class Producto extends Component {
         const { id, nombreProducto, contenido, enCarrito } = this.props.product;
 
         return (
-            <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                <div className="card">
-                    <ContextConsumer>
-                        {(value) => (
-                            <div className="img-container p-5"
-                                onClick={() =>
-                                    value.manejoDetalle(id)}
-                            >
-                                <Link to="/detalleProducto"><p>{contenido}</p></Link>
-                                <button classsName="cart-btn"
-                                    disabled=
-                                    {enCarrito ? true : false} onClick={() => {
-                                        value.agregarAlCarrito(id)
-                                    }}>
-                                    {enCarrito ?
-                                        (<p ClassName="text-capitaliza mb-0 "
-                                            disabled>
-                                            en el carrito
-                                </p>) :
-                                        (<i className="fas fa-cart-plus"></i>)}
-                                    Agregar
-                            </button>
-                            </div>
-                        )}
+            <div>
+    {this.state.productos.map(x => <div className="col-10 mx-auto col-md-6 my-3">
+        <div className="card" style={{ marginLeft: 15 }}>
+            <ContextConsumer>
+                {(value) => (
+                    <div className="img-container p-5"
+                        onClick={() =>
+                            value.manejoDetalle(id)}
+                    >
 
-                    </ContextConsumer>
-                </div>
-            </div >
+
+                        <Link to="/detalleProducto"><p>{x.Contenido}</p></Link>
+
+                        {/* 
+                <button classsName="cart-btn" 
+                    disabled = 
+                    {enCarrito?true : false} onClick={()=> {
+                        value.agregarAlCarrito(id)}}>
+                    {enCarrito?
+                    (<p ClassName="text-capitaliza mb-0 " 
+                    disabled> 
+                    en el carrito
+                    </p>) : 
+                    (<i className="fas fa-cart-plus"></i>)}
+                    Agregar
+                </button>
+                    */}
+
+                    </div>
+                )}
+
+            </ContextConsumer>
+
+             <div className="card-footer d-flex justify-content-between">
+                 <p className="align-self-center mb-0">{x.CodigoProducto}</p>
+                 <h6 className="font-bold mb-0">
+                     {x.NombreProducto}</h6>
+             </div>
+
+         </div>
+     </div>)}
+</div>
+//     )
+            // <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+            //     <div className="card">
+            //         <ContextConsumer>
+            //             {(value) => (
+            //                 <div className="img-container p-5"
+            //                     onClick={() =>
+            //                         value.manejoDetalle(id)}
+            //                 >
+            //                     <Link to="/detalleProducto"><p>{contenido}</p></Link>
+            //                     <button classsName="cart-btn"
+            //                         disabled=
+            //                         {enCarrito ? true : false} onClick={() => {
+            //                             value.agregarAlCarrito(id)
+            //                         }}>
+            //                         {enCarrito ?
+            //                             (<p ClassName="text-capitaliza mb-0 "
+            //                                 disabled>
+            //                                 en el carrito
+            //                     </p>) :
+            //                             (<i className="fas fa-cart-plus"></i>)}
+            //                         Agregar
+            //                 </button>
+            //                 </div>
+            //             )}
+
+            //         </ContextConsumer>
+            //     </div>
+            // </div >
             // <ContextConsumer>
             //     {(value) => (
             //         <div className="img-container p-5"
